948163040d8973e4c0e4261d49bf47c134e445b7
Fatima <fatimasohailhunzai@gmail.com>
2024-09-16 19:54:56 -0400

    Updated GiftTax file

--- java-mini-exercises/GiftTax.java
+++ java-mini-exercises/GiftTax.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
-
+//A gift is a transfer of property to another person against no compensation
+// or payment. If the total value of the gifts you receive from the same donor
+// in the course of 3 years is €5,000 or more, you must pay gift tax.
+//
+//When a gift is given by a close relative or a family member, the amount of
+// gift tax is determined by the following table (source vero.fi):
+//
+//Value of gift	    Tax at the lower limit	  Tax rate(%) for exceeding part
+//5 000 — 25,000	        100	                        8
+//25 000 — 55,000	        1700	                    10
+//55 000 — 200,000	        4700	                    12
+//200 000 — 1,000,000	    22100	                    15
+//1 000 000 — above         1,42,100	                17
+//For example 6000€ gift implies 180€ of gift tax (100 + (6000-5000) * 0.08),
+// and 75000€ gift implies 7100€ of gift tax (4700 + (75000-55000) * 0.12).
+//
+//Write a program that calculates the gift tax for a gift from a close relative
+// or a family member. This is how the program should work:
 public class GiftTax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
