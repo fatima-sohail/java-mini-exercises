65f3ab153237e1243bfcf4b982dbcfc9dd05d610
Fatima <fatimasohailhunzai@gmail.com>
2024-11-13 17:49:53 -0500

    added OOP exercises

---
+++ java-mini-exercises/oopMultiplier/Main.java
@@ -0,0 +1,7 @@
+package oopMultiplier;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java-mini-exercises/oopMultiplier/Multiplier.java
@@ -0,0 +1,4 @@
+package oopMultiplier;
+
+public class Multiplier {
+}
