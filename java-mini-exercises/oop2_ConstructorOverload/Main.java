6f86842baa403a41ab66f38227d5246e197aa083
Fatima <fatimasohailhunzai@gmail.com>
2024-11-28 11:03:09 -0500

    added more oop exercises

---
+++ java-mini-exercises/oop2_ConstructorOverload/Main.java
@@ -0,0 +1,13 @@
+package oop2_ConstructorOverload;
+
+public class Main {
+    public static void main(String[] args) {
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
+    }
+}
