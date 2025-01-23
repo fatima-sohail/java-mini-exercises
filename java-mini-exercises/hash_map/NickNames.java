ebe89669277182a80b7349e891125fa6eba1d36d
Fatima <fatimasohailhunzai@gmail.com>
2025-01-23 12:42:07 -0500

    added more hashmap exercises

--- java-mini-exercises/hash_map/NickNames.java
+++ java-mini-exercises/hash_map/NickNames.java
@@ -5,22 +5,26 @@ import java.util.HashMap;
 public class NickNames {
     public static void main(String[] args) {
         //create a hash map<String, String> obj
-        
-        //store the following names(key) and their nicknames(value) in the above hashmap
+
+        //using put()store the following names(key) and their nicknames(value) in hashmap
         //and use only lower case letters:
         //matthew's nickname is matt
         //michael's nickname is mix
         //arthur's nickname is artie
 
-        //get mathew's nick name from the hasmap and print it
+        //retreive mathew's nickname from the hashmap using get() and print it
 
         HashMap<String, String> nickNames = new HashMap<>();
         nickNames.put("mathew", "matt");
         nickNames.put("micheal", "mix");
         nickNames.put("arthur", "artie");
 
-        nickNames.get("mathew");
+        //nickNames.get("mathew");
         System.out.println(nickNames.get("mathew"));
+
+        //if the hash map doesn't contain the key used for the search, its get method returns
+        //a null reference
+        System.out.println(nickNames.get("artuu"));
 
     }
 }
---
+++ java-mini-exercises/hash_map_use_obj_as_hashmap_value_library/Main.java
@@ -0,0 +1,52 @@
+package hash_map_use_obj_as_hashmap_value_library;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        //create a hashmap obj/hashmap table with key type `String` and value type `Book`
+        HashMap<String, Book> directory = new HashMap<>();
+
+        //create objs of value--> Book first to put it in the hashmap
+        Book harryPotter = new Book("Harry Potter", ".....", 2000);
+        Book cinderella = new Book("Cinderella", "...", 1980);
+
+        //store the key-value pair using `put()` and retreive them using `get()`
+        //store the book by its key and value --> `key` type is the String name,
+        // and `value` type is Book
+        directory.put(harryPotter.getName(), harryPotter);
+        directory.put(cinderella.getName(), cinderella);
+
+        //retrieve by: hashmapObj.get(key) --> key is String name--> "Harry Potter"
+        System.out.println(directory.get("Harry Potter"));
+        System.out.println();
+        System.out.println(directory.get("Cinderella"));
+        System.out.println();
+
+        //retrieve just the field 'content' from cinderella
+        System.out.println("Retrieve just the field `content` from Cinderella: ");
+        System.out.println(directory.get("Cinderella").getContent());
+
+
+        //OR first store it in a variable and print that variable
+        //Book hp2 = directory.get("Harry Potter");
+        //System.out.println(hp2);
+
+        Book sw = directory.get("Snow White");
+        System.out.println(sw); //returns null cz it doesn't exist in the above hashmap table
+
+        //print the directory
+        System.out.println();
+        System.out.println(directory);
+
+        //change the name of harry Potter to something else using set() and print the haspmap table
+        Book hpReference = directory.get("Harry Potter");
+        hpReference.setName("SomeOtherBook");
+
+
+        System.out.println();
+        System.out.println("Reprinting the updated directory:");
+        System.out.println(directory);
+
+    }
+}
