07d42f4244271425e2de6345297e2ff92dda2f53
Fatima <fatimasohailhunzai@gmail.com>
2025-02-25 12:18:46 -0500

    added more exercises

---
+++ java-mini-exercises/inheritance_task1_ABC_2_parts/Main.java
@@ -0,0 +1,21 @@
+package inheritance_task1_ABC_2_parts;
+
+public class Main {
+    public static void main(String[] args){
+        //Part 1:
+//        A a = new A();
+//        B b = new B();
+//        C c = new C();
+//
+//        a.a();
+//        b.b();
+//        c.c();
+
+        //Part 2:
+        C c = new C();
+
+        c.a();
+        c.b();
+        c.c();
+    }
+}
