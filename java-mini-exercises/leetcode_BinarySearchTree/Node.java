4bd293589bc4fe54f88d6ecb08b1fddc6c4f9dc8
Fatima <fatimasohailhunzai@gmail.com>
2025-05-03 15:07:49 -0400

    added more files

---
+++ java-mini-exercises/leetcode_BinarySearchTree/Node.java
@@ -0,0 +1,15 @@
+package leetcode_BinarySearchTree;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int value){
+        this.data = value;
+        left = null;
+        right = null;
+    }
+
+
+}
